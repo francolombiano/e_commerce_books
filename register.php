b3acfd4c8416a4aa04bf531dcbb1a3bcbdae7046
Roberto <rquesadarad@yahoo.fr>
2024-04-02 01:56:16 +0200

    formulaire d'inscription avec verification en PHP pas termine

--- register.php
+++ register.php
@@ -10,10 +10,72 @@ if (!empty($_SESSION['user'])) {
 
 if (!empty($_POST)) // l'envoi du Formulaire (button "S'inscrire" ) 
 {
-  debug($_POST);
+  // debug($_POST);
+
+
+  $verif = true;
+  foreach ($_POST as $value) {
+
+
+    if (empty($value)) {
+
+      $verif = false;
+    }
+  }
+  $info = '';
+
+  if (!$verif) {
+
+    // debug($_POST);
+
+    $info = alert("Veuillez renseigner tout les champs", "danger");
+  } else {
+
+
+    // debug($_POST);
+    $firstName = isset($_POST['firstName']) ? $_POST['firstName'] : null;
+    $lastName = isset($_POST['lastName']) ? $_POST['lastName'] : null;
+    $tel = isset($_POST['tel']) ? $_POST['tel'] : null;
+    $email = isset($_POST['email']) ? $_POST['email'] : null;
+    $password = isset($_POST['password']) ? $_POST['password'] : null;
+    $confirmPassword = isset($_POST['confirmPassword']) ? $_POST['confirmPassword'] : null;
+    $civility = isset($_POST['civility']) ? $_POST['civility'] : null;
+    $cpostal = isset($_POST['cpostal']) ? $_POST['cpostal'] : null;
+    $ville = isset($_POST['ville']) ? $_POST['ville'] : null;
+    $pays = isset($_POST['pays']) ? $_POST['pays'] : null;
+
+
+    if (strlen($firstName) < 2 || preg_match('/[0-9]+/', $firstName)) {
+
+      $info = alert("Le prénom n'est pas valide.", "danger");
+    }
+
+    if (strlen($lastName) < 2 || preg_match('/[0-9]+/', $lastName)) {
+
+      $info .= alert("Le nom n'est pas valide.", "danger");
+    }
+
+    if (!preg_match('#^[0-9]+$#', $tel) || strlen($tel) > 10 || !trim($tel)) {
+
+      $info .= alert("Le Téléphone n'est pas valide.", "danger");
+    }
+    if (strlen($email) > 50 || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+      $info .= alert("L'email n'est pas valide.", "danger");
+    }
+    if (strlen($password) < 5 || strlen($password) > 15) {
+
+      $info .= alert("Le mot de passe n'est pas valide.", "danger");
+    }
+    if ($password !== $confirmPassword) {
+
+      $info .= alert("Le mot de passe et la confirmation doivent être identique.", "danger");
+    }
+    if ($civility != 'femme' && $civility != 'homme' && $civility != 'autre') {
+      $info .= alert("La civilité n'est pas valide.", "danger");
+    }
+  }
 }
-
-
 
 
 
@@ -23,16 +85,21 @@ require_once "inc/header.inc.php";
 ?>
 
 <main class="bg-register">
+
   <!-- Image d'en-tête contactez-nous -->
   <section class="affiche-inscription">
+
     <div class="text-center text-white col-sm-12">
       <h1 class="titre-3 display-3">Registre!</h1>
       <i class="bi bi-chevron-down down"></i>
+
+
+
     </div>
   </section>
 
   <!-- Formulaire de contact -->
-  <section class="ecrivez-nous p-5">
+  <section class="ecrivez-nous p-5"> <?= $info; ?>
     <form action="#" method="post" class="w-50 mx-auto p-3 text-white rounded-5 formV border p-5 col-sm-12 col-md-8">
 
       <div class="p-3 inputs col-sm-12">
